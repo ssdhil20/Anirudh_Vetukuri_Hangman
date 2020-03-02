53d5a2204e37f8275117ac073af8bc3f652aa4a9
Sunny <60932615+ssdhil20@users.noreply.github.com>
2020-03-02 11:09:53 -0500

    Update twogroupcode.py

--- twogroupcode.py
+++ twogroupcode.py
@@ -21,3 +21,37 @@ def checkLetter():
             '''
     print(updatedSpaces)
     print(word)
+    
+    import random
+WORDLIST = [""]
+updatedWord = ""
+
+def initialize():
+    global updatedWord
+    global SECRET
+    WORDLIST = ['one','two','three','four','five','six','seven','eight','nine','ten']
+    SECRET = random.choice(WORDLIST)
+    updatedWord = [('_'*len(SECRET))]
+   
+    print(updatedWord)
+    print SECRET
+
+def getLetter():
+    global letter
+    letter = str(raw_input('ENTER YOUR GUESS: '))
+    print ('YOUR GUESS: ' + letter)
+
+def fillLetter():
+    pos = SECRET.find(letter)
+    updatedWord[pos] = letter
+    
+def ifWon():
+    if updatedWord == SECRET:
+        print ('You Won!')
+    else: 
+        getLetter()
+        
+def main():
+    
+    initialize()
+    getLetter()
